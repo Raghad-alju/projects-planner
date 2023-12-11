cb5aa65f1abace44891aaa8b3c1d3e6130b97c1a
raghad77 <raghad.alju@outlook.com>
2023-12-11 23:04:08 +0300

    p

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,9 @@
 //this is  a new change
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState, useRef } from 'react';
 import ProjectDetail from './ProjectDetail';
 
-const box = `mr-5 ml-90 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 `
+const box = ` mr-5 ml-90 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 `
 const title = `mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white`
 
 export default function Project({ visibility, projectList, handleVisibility,updateProjectList,handleProjectList }) {
@@ -12,9 +12,9 @@ export default function Project({ visibility, projectList, handleVisibility,upda
     var today = new Date();
     var Due;
     const dialogRef = useRef();
-    var colorState={red:'',yellow:'',green:''};
+    
     //const [leftDays,setLeftDays] =useState()
-   
+  
     function handleDate(date) {
        
 
@@ -32,12 +32,12 @@ export default function Project({ visibility, projectList, handleVisibility,upda
   
         if (Due > 0) {
             if (Due > 7){
-               
-               
+                if(Math.floor(Due / 7)===1)
                 return `${Math.floor(Due / 7)} week`;
+                else
+                return `${Math.floor(Due / 7)} weeks`
             }
             else{
-            
                 return `${Due} days`;
             }
         } else if (Due < 0) {
@@ -97,27 +97,29 @@ export default function Project({ visibility, projectList, handleVisibility,upda
         setDoneProjects(doneProjects.filter(p=>p.id !== id))
         updateProjectList(id)
     }
+
     return (
         <>
 
             <div hidden={visibility} className='' >
-                <div className="grid grid-cols-1 md:grid-cols-1 p-6  sm:mx-72 justify-center mt-10  bg-gradient-to-br from-orange-400 to-orange-300 rounded-2xl">
-                <div className='flex text-2xl font-bold pb-6 text-white'>Current Projects</div>
-                    <div className={` grid grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-white rounded-xl border border-3 border-dashed border-gray-700  w-[60rem] ${projectList.length==0?'h-48':'h-auto'} `} onDrop={(e)=>handleOnDrop(e,'current')} onDragOver={handleDragOver}>
+                <div className="grid grid-cols-1 md:grid-cols-1 p-6  md:mx-72 justify-center mt-10  bg-gradient-to-br from-orange-400 to-orange-300 rounded-2xl">
+                <div className='  flex text-2xl font-bold pb-6 text-white'>Current Projects</div>
+                    <div className={` grid grid-cols-1 sm:grid-cols-3 gap-4 p-4 bg-white rounded-xl border border-3 border-dashed border-gray-700  w-auto ${projectList.length==0?'h-48':'h-auto'} `} onDrop={(e)=>handleOnDrop(e,'current')} onDragOver={handleDragOver}>
 
                         {projectList.map((project) => {
+                            //if(dialogRef.current===null)return
                             return (
-
-
+                             
                                 <div key={project.id} className={box+'hover:cursor-grab shadow-red-300 bg-gradient-to-br from-amber-100 to-orange-200 '} draggable onDragStart={(e)=>handleOnDrag(e,project)}>
                                     <ProjectDetail ref={dialogRef} pId={projectId} projectList={projectList} handleVisibility={handleVisibility}  deleteProject={deleteProject} />
 
-                                    <h5 className={title+' text-orange-800'}>{project.title}</h5>
+                                    <h5 className={title+' text-orange-700'}>{project.title}</h5>
                                     <p className="line-clamp-2 font-normal text-gray-700 ">{project.desc}</p>
 
-                                    <p  className={`inline-flex px-1 rounded-md font-normal text-gray-700 bg-${colorState} `} >{handleDate(project.endDue) === '0' ? `Due today` : `Due in ${handleDate(project.endDue)}`}</p>
+                                    <p  className={`inline-flex px-1 rounded-md font-normal text-gray-800 ${handleDate(project.endDue) === '0'? 'bg-red-400' : handleDate(project.endDue).substr(-5) === 'weeks'?'bg-green-300':'bg-yellow-300'}`} >{handleDate(project.endDue) === '0' ? `Due today` : `Due in ${handleDate(project.endDue)}`}</p>
                                     <br/>
-                                    <button className='' onClick={() => {if(dialogRef.current!=null)dialogRef.current.showModal(); setProjectId(project.id)}}>view detail</button>
+                                    <button className=' bg-gradient-to-br from-pink-500 to-orange-500 py-1 px-2 mx-auto mt-3 text-white rounded-lg hover:bg-gradient-to-br hover:from-orange-400 hover:to-pink-400' onClick={() => {if(dialogRef.current!=null)dialogRef.current.showModal(); setProjectId(project.id)}}>
+                                        View details</button>
                                 </div>
 
                             )
@@ -126,7 +128,7 @@ export default function Project({ visibility, projectList, handleVisibility,upda
                        
                     </div>
                     <div className='flex text-2xl font-bold py-6'>On Progress</div>
-                    <div className={` grid grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-white rounded-xl border border-3 border-dashed border-gray-700 w-[60rem] ${doingProjects.length==0?'h-48':'h-auto'} `} onDrop={(e)=>handleOnDrop(e,'doing')} onDragOver={handleDragOver}>
+                    <div className={` grid grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-white rounded-xl border border-3 border-dashed border-gray-700 w-auto ${doingProjects.length==0?'h-48':'h-auto'} `} onDrop={(e)=>handleOnDrop(e,'doing')} onDragOver={handleDragOver}>
                         {doingProjects.map((project)=>{return(
                             <div key={project.id} className={box+'hover:cursor-grab'} draggable onDragStart={(e)=>handleOnDrag(e,project)}>
                                     <ProjectDetail ref={dialogRef} pId={projectId} projectList={doingProjects} handleVisibility={handleVisibility}  deleteProject={deleteProject}/>
@@ -142,7 +144,7 @@ export default function Project({ visibility, projectList, handleVisibility,upda
 
                         </div>
                         <div className='flex text-2xl font-bold py-6'>Done</div>
-                        <div className={` grid grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-white rounded-xl border border-3 border-dashed border-gray-700 w-[60rem] ${doneProjects.length==0?'h-48':'h-auto'} `} onDrop={(e)=>handleOnDrop(e,'done')} onDragOver={handleDragOver}>
+                        <div className={` grid grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-white rounded-xl border border-3 border-dashed border-gray-700 w-auto ${doneProjects.length==0?'h-48':'h-auto'} `} onDrop={(e)=>handleOnDrop(e,'done')} onDragOver={handleDragOver}>
                         {doneProjects.map((project,index)=>{return(
                             <div key={project.id} className={box+'hover:cursor-grab'} draggable onDragStart={(e)=>handleOnDrag(e,project)}>
                                     <ProjectDetail ref={dialogRef} pId={projectId} projectList={doneProjects} handleVisibility={handleVisibility}  deleteProject={deleteProject} />
